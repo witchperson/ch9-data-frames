200e5a1268cfde8c1d250972a64d756c03c634d6
Joel Ross <joelross@uw.edu>
2018-01-15 09:16:38 -0800

    Add exercises

---
+++ exercise-5/exercise.R
@@ -0,0 +1,32 @@
+# Exercise 5: large data sets: Baby Name Popularity Over Time
+
+# Read in the female baby names data file found in the `data` folder into a 
+# variable called `names`. Remember to NOT treat the strings as factors!
+
+
+# Create a data frame `names.2013` that contains only the rows for the year 2013
+
+
+# What was the most popular female name in 2013?
+
+
+# Write a function `MostPopular` that takes in a year as a value and returns
+# the most popular name in that year
+
+
+# What was the most popular female name in 1994?
+
+
+# Write a function `NumberInMillion` that takes in a name and a year, and returns
+# statistically how many babies out of 1 million born that year have that name. 
+# Hint: get the popularity percentage, and take that percentage out of 1 million.
+
+
+# How many babies out of 1 million had the name 'Laura' in 1995?
+
+
+# How many babies out of 1 million had your name in the year you were born?
+
+
+## Consider: what does this tell you about how easy it is to identify you with 
+## just your name and birth year?
