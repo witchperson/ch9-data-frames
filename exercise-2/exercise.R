3c0482512e8407ee968381eee964d54d386eabd8
Kangwoo Choi <kangwooc@gmail.com>
2018-01-23 11:40:55 -0800

    exercise-3

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,28 +2,28 @@
 
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100")
 # Hint: use the `paste()` function and vector recycling
-
+employees <- c(paste("Employee", 1:100))
 
 # Create a vector of 100 random salaries for the year 2016
 # Use the `runif()` function to pick random numbers between 40000 and 50000
-
+employees.salaries.2016 <- c(runif(100, 40000, 50000))
 
 # Create a vector of 100 salaries in 2017 that have changed from 2016
 # Use `runif()` to add a random number between -5000 and 10000 to each of 2016's
 # salaries (the negative number means that a salary may have decreased!)
-
+employees.salaries.2017 <- c(employees.salaries.2016+runif(100, -5000, 10000))
 
 # Create a data frame 'salaries' by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
-
+salaries <- data.frame(employees, employees.salaries.2016, employees.salaries.2017, stringsAsFactors = 'FALSE')
 
 # Create a column 'change' that stores each person's change in salary between
 # 2016 and 2017
-
-
+salaries$change <- salaries$employees.salaries.2017 - salaries$employees.salaries.2016
+View(salaries)
 # Create a column 'got.raise' that is TRUE if the person got a raise (their
 # salary went up)
-
+got.raise <- salaries$change[change>0]
 
 
 ### Retrieve values from your data frame to answer the following questions
@@ -31,25 +31,25 @@
 ### cell rather than the whole row!)
 
 # What was the 2017 salary of employee 57
-
+salaries[57, 3]
 
 # How many employees got a raise?
-
+length(got.raise)
 
 # What was the dollar value of the highest raise?
-
+max(salaries$change)
 
 # What was the "name" of the employee who received the highest raise?
 
 
 # What was the largest decrease in salaries between the two years?
-
+min(salaries$change)
 
 # What was the name of the employee who recieved largest decrease in salary?
-
+salaries[,1]
 
 # What was the average salary change?
-
+mean(salaries$change)
 
 # For people who did not get a raise, how much money did they lose on average?
 
@@ -58,4 +58,4 @@
 ## how you generated the salaries?
 
 # Write a .csv file of your salary data to your working directory
-
+write.csv()
--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -2,42 +2,47 @@
 
 # Load R's "USPersonalExpenditure" dataset using the `data()` function
 # This will produce a data frame called `USPersonalExpenditure`
-
+data("USPersonalExpenditure")
 
 # The variable `USPersonalExpenditure` is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+is.data.frame(USPersonalExpenditure)
+USPersonalExpenditure
 
 # Luckily, you can pass the USPersonalExpenditure variable as an argument to the
 # `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
-
+us.exp <- data.frame(USPersonalExpenditure)
+is.data.frame(us.exp)
 # What are the column names of your dataframe?
+colnames(us.exp)
 
 
 ## Consider: why are they so strange? Think about whether you could use a number 
 ## like 1940 with dollar notation!
 
 # What are the row names of your dataframe?
-
+rownames(us.exp)
 
 # Add a column "category" to your data frame that contains the rownames
-
+us.exp$category <- rownames(us.exp)
 
 # How much money was spent on personal care in 1940?
-
+us.exp['Personal Care', 'X1940']
 
 # How much money was spent on Food and Tobacco in 1960?
-
+us.exp['Food and Tobacco', 'X1960']
 
 # What was the highest expenditure category in 1960?
-
+us.exp$category[max(us.exp$X1960) == us.exp$X1960]
 
 # Define a function `HighestCategory` that takes in a year as a parameter, and
 # returns the highest spending category of that year
-
+HighestCategory <- function(year) {
+  highest <- us.exp$category[max(us.exp[[paste0('X',year)]]) == us.exp[[paste0('X',year)]]]
+  return (highest)
+}
 
 # Using your function, determine the highest spending category of each year
 # Hint: use the `sapply()` function to apply your function to a vector of years
